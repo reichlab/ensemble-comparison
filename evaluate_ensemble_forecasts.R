3ea99f031310cd0b9e1c7187ba6df5392ee58348
Li Shandross <lshandross@outlook.com>
2023-08-24 15:42:32 -0400

    Update evaluate_ensemble_forecasts.R

--- evaluate_ensemble_forecasts.R
+++ evaluate_ensemble_forecasts.R
@@ -15,34 +15,27 @@ flu_dates_22_23 <- as.Date("2022-10-17") + weeks(0:30)
 all_flu_dates <- c(flu_dates_21_22, flu_dates_22_23)
 
 # Read in data
-flu_mean_21_22 <- readr::read_rds("data/flu_mean-ensemble_21-22.rds", "xz", compression = 9L)
-flu_mean_22_23 <- readr::read_rds("data/flu_mean-ensemble_22-23.rds", "xz", compression = 9L)
-flu_median_21_22 <- readr::read_rds("data/flu_median-ensemble_21-22.rds", "xz", compression = 9L)
-flu_median_22_23 <- readr::read_rds("data/flu_median-ensemble_22-23.rds", "xz", compression = 9L)
-flu_linear_pool_21_22 <- readr::read_rds("data/flu_linear_pool-ensemble_21-22.rds", "xz", compression = 9L)
-flu_linear_pool_22_23 <- readr::read_rds("data/flu_linear_pool-ensemble_22-23.rds", "xz", compression = 9L)
+flu_truth_all <- readr::read_rds("data/flu_truth_all.rds")
+flu_mean_21_22 <- readr::read_rds("data/flu_mean-ensemble_21-22.rds")
+flu_mean_22_23 <- readr::read_rds("data/flu_mean-ensemble_22-23.rds")
+flu_median_21_22 <- readr::read_rds("data/flu_median-ensemble_21-22.rds")
+flu_median_22_23 <- readr::read_rds("data/flu_median-ensemble_22-23.rds")
+flu_linear_pool_21_22 <- readr::read_rds("data/flu_linear_pool-ensemble_21-22.rds")
+flu_linear_pool_22_23 <- readr::read_rds("data/flu_linear_pool-ensemble_22-23.rds")
+
+all_flu_df <- sort(paste("flu", rep(c("mean", "median", "linear_pool", "linear_pool_lognorm"), 2), c(rep("21_22", 4), rep("22_23", 4)), sep="_"))
+
+flu_truth_21_22 <- flu_truth_all |>
+  dplyr::filter(target_end_date < "2022-08-01")
+flu_truth_22_23 <- flu_truth_all |>
+  dplyr::filter(target_end_date > "2022-08-01")
 
 
-flu_21_22_truth <- covidHubUtils::load_truth(
-    truth_end_date="2023-05-15",
-    data_location="covidData",
-    hub = "FluSight"
-)
-
-origin_dates <-  "2023-05-15"
-# Query truth
-truth_data <- zoltar_connection |> 
-  do_zoltar_query(project_url, query_type ="truth", 
-#                  units = c("loc1", "loc2"), 
-#                  targets = "week ahead incident hospitalizations",
-                  timezeros = origin_dates, 
-                  types = "quantile") |>
-  dplyr::mutate(model_id = "flu-truth", .before = 1) |>
-  tidyr::separate(target, sep=2, convert=TRUE, into=c("horizon", "target")) 
-# need to match forecasts format
-# also must have: model, forecast_date, location, horizon, temporal_resolution, target_variable, horizon, target_end_date
-# to use score_forecasts() function
-                  
 # Score Forecasts
+# forecast columns: model, forecast_date, location, horizon, temporal_resolution,
+#                   target_variable, target_end_date, type, quantile, value
+# truth_columns: model, target_variable, target and date, location, value
 flu_mean_21_22_scores <- flu_mean_21_22 |> 
-  covidHubUtils::score_forecasts(return_format="wide", truth=flu_21_22_truth)
+  covidHubUtils::score_forecasts(flu_truth_21_22, return_format="wide", use_median_as_point=TRUE)
+
+View(flu_mean_21_22_scores)
