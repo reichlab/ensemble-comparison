2430641ddbb50ddbe80dba053b8cfe99f2681d79
Li Shandross <lshandross@outlook.com>
2023-08-25 16:44:50 -0400

    score flu forecasts

--- evaluate_ensemble_forecasts.R
+++ evaluate_ensemble_forecasts.R
@@ -23,7 +23,12 @@ flu_median_22_23 <- readr::read_rds("data/flu_median-ensemble_22-23.rds")
 flu_linear_pool_21_22 <- readr::read_rds("data/flu_linear_pool-ensemble_21-22.rds")
 flu_linear_pool_22_23 <- readr::read_rds("data/flu_linear_pool-ensemble_22-23.rds")
 
-all_flu_df <- sort(paste("flu", rep(c("mean", "median", "linear_pool", "linear_pool_lognorm"), 2), c(rep("21_22", 4), rep("22_23", 4)), sep="_"))
+
+# all_flu_df <- sort(paste("flu", rep(c("mean", "median", "linear_pool", "linear_pool_lognorm"), 2), c(rep("21_22", 4), rep("22_23", 4)), sep="_"))
+flu_files <- list.files(path="data", pattern="ensemble", full.names=TRUE)
+flu_forecasts_all <- purrr::map_dfr(flu_files, .f=read_rds) |>
+    dplyr::select(model, forecast_date, location, horizon, temporal_resolution, 
+                  target_variable, target_end_date, type, quantile, value)
 
 flu_truth_21_22 <- flu_truth_all |>
   dplyr::filter(target_end_date < "2022-08-01")
@@ -37,5 +42,8 @@ flu_truth_22_23 <- flu_truth_all |>
 # truth_columns: model, target_variable, target and date, location, value
 flu_mean_21_22_scores <- flu_mean_21_22 |> 
   covidHubUtils::score_forecasts(flu_truth_21_22, return_format="wide", use_median_as_point=TRUE)
+  
+flu_scores_all <- flu_forecasts_all |> 
+  covidHubUtils::score_forecasts(flu_truth_all, return_format="wide", use_median_as_point=TRUE)
 
-View(flu_mean_21_22_scores)
+readr::write_rds(flu_scores_all, "data/flu_scores_all.rds", "xz", compression = 9L)
