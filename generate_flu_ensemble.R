2a387babab48618f9825e3222c69d73f30a7c20b
Li Shandross <57642277+lshandross@users.noreply.github.com>
2023-09-13 16:03:45 -0400

    Change ensemble generation script to hubverse format

--- generate_flu_ensemble.R
+++ generate_flu_ensemble.R
@@ -3,57 +3,47 @@ library(lubridate)
 library(readr)
 library(stringr)
 library(hubEnsembles)
+library(hubUtils)
 
-# getwd()
-# flusight_truth_path <- "../Flusight-forecast-data/data-truth/truth-Incident Hospitalizations.csv"
-# current_truth <- readr::read_csv(flusight_truth_path)
-
-# reference_date <- lubridate::ceiling_date(Sys.Date(), "week") - days(1)
-reference_date <- "2023-05-08"
-flusight_data_path <- "../Flusight-forecast-data/data-forecasts"
-model_names <- list.dirs(flusight_data_path, full.names=FALSE) 
-current_forecast_files <- list.files(flusight_data_path, full.names=TRUE, recursive=TRUE, pattern=reference_date) 
-
-current_forecasts_raw <- purrr::map_dfr(current_forecast_files, .f=function(path) {
-  name_start <- stringr::str_locate(path, reference_date)[1, 1] + 11
-  model_name <- stringr::str_sub(path, start=name_start, end=stringr::str_length(path) - 4)
-  readr::read_csv(path) |>
-    dplyr::mutate(model = model_name, .before = 1)
-}) 
-
-# reference_date will replace forecast_date
-task_id_cols <- c("forecast_date", "location", "horizon", "target")
+reference_date <- lubridate::ceiling_date(Sys.Date(), "week") - days(1)
+task_id_cols <- c("reference_date", "location", "horizon", "target", "target_end_date")
   
-current_forecasts_formatted <- current_forecasts_raw |>
-  dplyr::filter(str_detect(model, "Flusight", negate=TRUE)) |> # remove baseline and ensemble
-  tidyr::separate(target, sep=" ", convert=TRUE, into=c("horizon", "target"), extra="merge") |>
-  # May need to merge `model` and `team` columns into `model_id` column
-  as_model_out_tbl(model_id_col = "model",
-                  output_type_col = "type",
-                  output_type_id_col = "quantile",
-                  value_col = "value",
-                  sep = "-",
-                  trim_to_task_ids = FALSE,
-                  hub_con = NULL,
-                  task_id_cols = task_id_cols,
-                  remove_empty = TRUE) #|>
+hub_path <- "../FluSight-forecast-hub"
+hub_con <- connect_hub(hub_path) 
+hub_con |>
+  dplyr::filter(
+    reference_date == current_ref_date, 
+    stringr::str_detect(model, "Flusight", negate=TRUE) # remove baseline and ensembles
+  ) |> 
+  dplyr::collect() |>
+  as_model_out_tbl() 
 
-names(current_forecasts_formatted)
 
 # generate median ensemble
-median_ensemble_outputs <- current_forecasts_formatted |>
-  hubEnsembles::simple_ensemble(agg_fun="median", model_id="Flusight-median", task_id_cols=task_id_cols) |>
+median_name <- "Flusight-median"
+median_ensemble_outputs <- current_forecasts |>
+  hubEnsembles::simple_ensemble(
+    agg_fun="median", 
+    model_id=median_name, 
+    task_id_cols=task_id_cols
+  ) |>
   dplyr::mutate(value = ifelse(value < 0, 0, value)) |>
-  dplyr::select(-model)
+  dplyr::select(-model_id)
 
-median_ensemble_path <- paste(flusight_data_path, "/Flusight-median/", reference_date, "-Flusight-median.csv", sep="") 
+median_ensemble_path <- paste(hub_path, "/model-outputs/", median_name, "/", reference_date, "-", median_name, ".csv", sep="") 
 readr::write_csv(median_ensemble_outputs, median_ensemble_path)
   
+
 # generate linear pool
-lop_normal_outputs <- current_forecasts_formatted |>
-  hubEnsembles::linear_pool(model_id="Flusight-lop_norm", tail_dist="norm", task_id_cols=task_id_cols) |>
+lop_norm_name <- "Flusight-lop_norm"
+lop_normal_outputs <- current_forecasts |>
+  hubEnsembles::linear_pool(
+    model_id=lop_norm_name, 
+    task_id_cols=task_id_cols
+  ) |>
   dplyr::mutate(value = ifelse(value < 0, 0, value)) |>
   dplyr::select(-model)
 
-lop_normal_path <- paste(flusight_data_path, "/Flusight-lop_norm/", reference_date, "-Flusight-lop_norm.csv", sep="") 
-readr::write_csv(lop_normal_outputs, lop_normal_path)
+lop_norm_ensemble_path <- paste(hub_path, "/model-outputs/", lop_norm_name, "/", reference_date, "-", lop_norm_name, ".csv", sep="") 
+readr::write_csv(lop_norm_outputs, lop_norm_path)
+
