6ddae882ac23912ce75d021f6fe05604f1b0d8cb
Li Shandross <57642277+lshandross@users.noreply.github.com>
2023-09-25 13:02:53 -0400

    Fixes to weekly ensemble generation script

--- generate_flu_ensemble.R
+++ generate_flu_ensemble.R
@@ -5,17 +5,19 @@ library(stringr)
 library(hubEnsembles)
 library(hubUtils)
 
-reference_date <- lubridate::ceiling_date(Sys.Date(), "week") - days(1)
+current_ref_date <- lubridate::ceiling_date(Sys.Date(), "week") - days(1)
 task_id_cols <- c("reference_date", "location", "horizon", "target", "target_end_date")
   
 hub_path <- "../FluSight-forecast-hub"
 hub_con <- connect_hub(hub_path) 
-hub_con |>
+current_forecasts <- hub_con |>
   dplyr::filter(
     reference_date == current_ref_date, 
-    stringr::str_detect(model, "Flusight", negate=TRUE) # remove baseline and ensembles
+    output_type == "quantile" ,
+    stringr::str_detect(model_id, "Flusight", negate=TRUE) # remove baseline and ensembles
   ) |> 
   dplyr::collect() |>
+  dplyr::mutate(output_type_id=as.numeric(output_type_id)) |>
   as_model_out_tbl() 
 
 
@@ -30,20 +32,20 @@ median_ensemble_outputs <- current_forecasts |>
   dplyr::mutate(value = ifelse(value < 0, 0, value)) |>
   dplyr::select(-model_id)
 
-median_ensemble_path <- paste(hub_path, "/model-outputs/", median_name, "/", reference_date, "-", median_name, ".csv", sep="") 
+median_ensemble_path <- paste(hub_path, "/model-output/", median_name, "/", current_ref_date, "-", median_name, ".csv", sep="") 
 readr::write_csv(median_ensemble_outputs, median_ensemble_path)
   
 
 # generate linear pool
 lop_norm_name <- "Flusight-lop_norm"
-lop_normal_outputs <- current_forecasts |>
+lop_norm_outputs <- current_forecasts |>
   hubEnsembles::linear_pool(
     model_id=lop_norm_name, 
     task_id_cols=task_id_cols
   ) |>
   dplyr::mutate(value = ifelse(value < 0, 0, value)) |>
-  dplyr::select(-model)
+  dplyr::select(-model_id)
 
-lop_norm_ensemble_path <- paste(hub_path, "/model-outputs/", lop_norm_name, "/", reference_date, "-", lop_norm_name, ".csv", sep="") 
+lop_norm_path <- paste(hub_path, "/model-output/", lop_norm_name, "/", current_ref_date, "-", lop_norm_name, ".csv", sep="") 
 readr::write_csv(lop_norm_outputs, lop_norm_path)
 
