5dc0274a36726fa3539fff58139e311155f34dfc
Li Shandross <57642277+lshandross@users.noreply.github.com>
2023-10-12 10:56:43 -0400

    Update generate_flu_ensemble.R

--- generate_flu_ensemble.R
+++ generate_flu_ensemble.R
@@ -13,17 +13,20 @@ hub_con <- connect_hub(hub_path)
 current_forecasts <- hub_con |>
   dplyr::filter(
     reference_date == current_ref_date, 
-    output_type == "quantile" ,
-    stringr::str_detect(model_id, "Flusight", negate=TRUE) # remove baseline and ensembles
+    stringr::str_detect(model_id, "FluSight", negate=TRUE) # remove baseline and ensembles
   ) |> 
   dplyr::collect() |>
-  dplyr::mutate(output_type_id=as.numeric(output_type_id)) |>
   as_model_out_tbl() 
 
 
+# QUANTILE ENSEMBLE
+quantile_forecasts <- current_forecasts |>
+  dplyr::filter(output_type == "quantile") |>
+  dplyr::mutate(output_type_id=as.character(as.numeric(output_type_id))) # ensures quantiles treated the same regardless of presence of trailing zeros
+  
 # generate median ensemble
-median_name <- "Flusight-median"
-median_ensemble_outputs <- current_forecasts |>
+median_name <- "FluSight-median"
+median_ensemble_outputs <- quantile_forecasts |>
   hubEnsembles::simple_ensemble(
     agg_fun="median", 
     model_id=median_name, 
@@ -32,20 +35,45 @@ median_ensemble_outputs <- current_forecasts |>
   dplyr::mutate(value = ifelse(value < 0, 0, value)) |>
   dplyr::select(-model_id)
 
-median_ensemble_path <- paste(hub_path, "/model-output/", median_name, "/", current_ref_date, "-", median_name, ".csv", sep="") 
-readr::write_csv(median_ensemble_outputs, median_ensemble_path)
+# median_ensemble_path <- paste(hub_path, "/model-output/", median_name, "/", current_ref_date, "-", median_name, ".csv", sep="") 
+# readr::write_csv(median_ensemble_outputs, median_ensemble_path)
   
-
-# generate linear pool
-lop_norm_name <- "Flusight-lop_norm"
-lop_norm_outputs <- current_forecasts |>
+# generate linear pool of quantiles (if desired)
+lop_norm_name <- "FluSight-lop_norm"
+lop_norm_outputs <- quantile_forecasts |>
+  dplyr::mutate(output_type_id=as.numeric(output_type_id)) |>
   hubEnsembles::linear_pool(
     model_id=lop_norm_name, 
     task_id_cols=task_id_cols
   ) |>
-  dplyr::mutate(value = ifelse(value < 0, 0, value)) |>
+  dplyr::mutate(
+    value = ifelse(value < 0, 0, value), 
+    output_type_id = as.character(output_type_id)
+  ) |>
+  dplyr::select(-model_id)
+ 
+ lop_norm_path <- paste(hub_path, "/model-output/", lop_norm_name, "/", current_ref_date, "-", lop_norm_name, ".csv", sep="") 
+ readr::write_csv(lop_norm_outputs, lop_norm_path)
+
+
+# PMF ENSEMBLE
+categorical_name <- "FluSight-categorical"
+categorical_forecasts <- current_forecasts |>
+  dplyr::filter(output_type == "pmf") |>
+  dplyr::filter(location != "78") |>
+  dplyr::group_by(reference_date, target, target_end_date, output_type) |> # create appropriate groups for `complete`
+  tidyr::complete(model_id, horizon, location, output_type_id, fill=list(value=-1)) # add in missing output_type_ids and fill the missing values with zero
+  
+categorical_ensemble_outputs <- categorical_forecasts |>
+  hubEnsembles::simple_ensemble(
+    agg_fun="mean", 
+    model_id=categorical_name, 
+    task_id_cols=task_id_cols
+  ) |>
   dplyr::select(-model_id)
 
-lop_norm_path <- paste(hub_path, "/model-output/", lop_norm_name, "/", current_ref_date, "-", lop_norm_name, ".csv", sep="") 
-readr::write_csv(lop_norm_outputs, lop_norm_path)
-
+ensemble_name <- "FluSight-ensemble"
+flusight_ensemble_outputs <- median_ensemble_outputs |>
+  dplyr::bind_rows(categorical_ensemble_outputs)
+flusight_ensemble_path <- paste(hub_path, "/model-output/", ensemble_name, "/", current_ref_date, "-", ensemble_name, ".csv", sep="") 
+readr::write_csv(flusight_ensemble_outputs, flusight_ensemble_path)
